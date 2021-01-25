0e3700e24ff8fd945c4b98d7b2b703c2be6344d1
unknown <clementgheist68@gmail.com>
2021-01-25 13:01:05 +0100

    Commit de fin de séance

---
+++ app/controllers/ex1.js
@@ -0,0 +1,40 @@
+import Controller from '@ember/controller';
+import {action} from '@ember/object';
+import {tracked} from "@glimmer/tracking";
+
+export default class Ex1Controller extends Controller {
+
+  @tracked info = "";
+  @tracked content = "entrez votre texte";
+  MAX = 100;
+
+  get style(){
+    if(this.MAX = this.content.length > 10){
+     return "alert-info";
+    }
+    else if(this.MAX - this.content.length < 0){
+      return "alert-danger";
+    }
+    else{
+      return "alert-warning";
+    }
+  }
+
+  get size(){
+    return this.MAX - this.content.lenght;
+  }
+
+  @action save(){
+    this.info = "Note sauvegardé"
+  }
+
+  @action clear(){
+  this.info = "";
+  this.content="";
+  }
+
+  @action update(info){
+    this.info = "Note modifiée";
+  }
+
+}
