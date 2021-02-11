1bbf9822e5e1d07dacd3b786e8a49136be21657f
unknown <clementgheist68@gmail.com>
2021-02-11 10:10:58 +0100

    Route step1

---
+++ app/routes/step1.js
@@ -0,0 +1,16 @@
+import Route from '@ember/routing/route';
+
+export default class Step1Route extends Route{
+
+  model(){
+    return
+    [...];
+  }
+
+  setupController(controller: model){
+
+
+  }
+
+}
+
