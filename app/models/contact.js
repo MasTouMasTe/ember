188045a4efb84c9859004367f6431e22236bee9e
moi <clementgheist68@gmail.com>
2021-02-22 15:02:41 +0100

    Modification de contact

---
+++ app/models/contact.js
@@ -0,0 +1,9 @@
+import Model, { attr } from '@ember-data/model';
+
+export default class ContactModel extends Model {
+  @belongsTo('category') category;
+
+  @attr('string') nom;
+  @attr('string') prenom;
+  @attr('string') email;
+}
