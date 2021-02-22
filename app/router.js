7b662d16996252af728f524a3dfc4ab09e135b85
moi <clementgheist68@gmail.com>
2021-02-22 15:01:16 +0100

    Ajout des routes

--- app/router.js
+++ app/router.js
@@ -6,4 +6,13 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('categories', function() {
+    this.route('contacts', function() {
+      this.route('add');
+      this.route('update');
+    });
+    this.route('add');
+    this.route('update');
+  });
+});
